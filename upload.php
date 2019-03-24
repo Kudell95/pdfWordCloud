074af042123b40a9ca942d961730361816a590ea
Matt <thormoth@live.com.au>
2019-03-24 13:16:03 +1000

    multiple uploads now working

--- upload.php
+++ upload.php
@@ -1,32 +1,28 @@
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+// $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
 
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+$totalFiles = count($_FILES['fileToUpload']['name']);
 
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sory, there was an error uploading your file.";
+for ( $i = 0;  $i < $totalFiles; $i++){
+    $tmpFilePath = $_FILES['fileToUpload']['tmp_name'][$i];
+
+    // if the file exists.
+    if($tmpFilePath != "")
+    {
+        $newPath = "./uploads/" . $_FILES['fileToUpload']['name'][$i];
+        if(move_uploaded_file($tmpFilePath, $newPath)){
+
+            // format file here.
+        }
     }
+
 }
+
+
 ?>
 
 
