12c408b4b47883834df5cdf58ef5d4e404063c66
Matt <kudell95@hotmail.com>
2019-03-28 10:16:42 +1000

    parses pdf to json

--- upload.php
+++ upload.php
@@ -1,13 +1,15 @@
 <?php
-
+header('Content-Type: application/json');
 // uses composer to sync packages
 
 include 'vendor/autoload.php';
 $dir = new RecursiveDirectoryIterator("./uploads/");
 $parser = new \Smalot\PdfParser\Parser();
+$words = array();
+
 
 $targetDir = 'uploads/';
-echo phpversion();
+// echo phpversion();
 
  if (!empty($_FILES)) {
     $targetFile = $targetDir.time().'-'. $_FILES['file']['name'];
@@ -17,17 +19,30 @@ echo phpversion();
  foreach ($dir as $fileinfo) {
    // if (!$fileinfo.isDot()) {
       $filename = $fileinfo->getFilename();
-      echo $fileinfo->getFilename() . "\n";
+      
+      // echo $fileinfo->getFilename() . "\n";
       $tmp = explode(".", $fileinfo->getFilename());
       $extension = end($tmp);
       if($extension == "pdf"){
          $pdf  = $parser->parseFile( $targetDir.$filename);
          $text = $pdf->getText();
-         file_put_contents("uploads/pdftext.txt", $text, FILE_APPEND);
-      
+         // trim($text, " \t");
+         // trim($text, " \n");
+
+         $temp_arr = explode(" ", $text);
+         $words =  array_merge($words, $temp_arr); // store every word in array words
+
+
+         // file_put_contents("uploads/pdftext.json", json_encode($words), FILE_APPEND);  
+         
       }
+
+        
    // }
 }
+
+// $words = preg_replace("\t","", $words);
+file_put_contents("uploads/pdftext.json", json_encode($words), FILE_APPEND);
  // loop through all files in uploads and parse them as text
 //  $pdf  = $parser->parseFile( "uploads/1553511324-test_pdf1.pdf" );
 //  $text = $pdf->getText();
@@ -35,5 +50,5 @@ echo phpversion();
 
 //transfer text to more workable file format like json or txt and format it in js
    
-    
+ 
 ?>
