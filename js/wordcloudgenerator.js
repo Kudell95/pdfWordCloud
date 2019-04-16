d12a085f633cdf97b7a756af4c0a4153196b1f2e
Matt <kudell95@hotmail.com>
2019-04-16 23:13:20 +1000

    fixed issues in wordcloudgenerator.js
    found some issues where certain browsers will cache the pdf and produce the same output everytime.

--- js/wordcloudgenerator.js
+++ js/wordcloudgenerator.js
@@ -1,34 +1,45 @@
 
+var count = 0;
+
 function generate(){
 
-
-
-  //FIXME: this sorta works but doesn't seem to update when new files are added.
-  loadJSON(function(response) {
-    // Parse JSON string into object
-      var words = JSON.parse(response);
+//found much better way to load json using jquery
+//found problem where the json file would get cached with some browsers, causing the file to not be updated when the script is run.
+  $.getJSON( "uploads/pdftext.json", function( words ) {
       console.log(words);
-   });
+});
+
+
+
+
+//   loadJSON(function(response) {
+//     // Parse JSON string into object
+//       var words = JSON.parse(response);
+//       // console.log(words);
+//       console.table(words.slice(0,1000));
+//       // console.table(words.slice(999,2000));
+//       // console.table(words.slice(1999,3000));
+//    });
   
   
-  //TODO: use the "Words" array to sort through and find most common one's
+//   //TODO: use the "Words" array to sort through and find most common one's
 
-  //format the common words in a two dimmensional array, i.e. ["word", "occurence"] so it can be used by wordcloud2.js
+//   //format the common words in a two dimmensional array, i.e. ["word", "occurence"] so it can be used by wordcloud2.js
 
-}; 
-
-
+// }; 
 
 
-function loadJSON(callback) {   
 
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("uploads/pdftext.json");
-  xobj.open('GET', 'uploads/pdftext.json', true);
-  xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-        }
-  };
-  xobj.send(null);  
+
+// function loadJSON(callback) {   
+
+//   var xobj = new XMLHttpRequest();
+//   xobj.overrideMimeType("uploads/pdftext.json");
+//   xobj.open('GET', 'uploads/pdftext.json', true);
+//   xobj.onreadystatechange = function () {
+//         if (xobj.readyState == 4 && xobj.status == "200") {
+//             callback(xobj.responseText);
+//         }
+//   };
+//   xobj.send(null);  
 }
