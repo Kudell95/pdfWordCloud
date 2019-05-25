3bff4b51ecc5aff9b09e73f115d2ba03f3510a3f
Matt <kudell95@Hotmail.com>
2019-05-25 21:32:29 +1000

    Started possible session solution

--- js/config.js
+++ js/config.js
@@ -1,7 +1,18 @@
 var FileDrop = true;
 var TextInput = true;
 
-
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
+    } else {
+     expires = "";
+    }
+    document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
+  }
+  
 
 //set the variables as bools and initialise for flip-flop
 window.onload = function(){
@@ -10,8 +21,15 @@ window.onload = function(){
     $("#dropInput").toggle(FileDrop);
     $("#textInput").toggle(TextInput);
     $.ajax({ url: 'clearFiles.php' });
+
+    // possible session method
+    // if(!document.cookie.split(';').filter((item) => item.trim().startsWith('SessionID=')).length) //check cookie existence on this pc
+    //     createCookie("SessionID", JSON.stringify(text), "10"); //create sessionId with random unique number, assigned to each user at window load 
     
 }
+
+
+
 
 
 function clearFiles(){
